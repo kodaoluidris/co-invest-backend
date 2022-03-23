c4cf96f56fe2ec09798e724a59e0bfd2e790a1cc
Adetayo Tiamiyu <adetayotiamiyu@gmail.com>
2022-03-23 14:09:55 +0100

    update on manage users logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,4 +12,18 @@ class UserController extends Controller
         $users = User::where('user_type_id', '!=', 2)->get();
         return $users;
     }
+
+    public function update_user_details(Request $request)
+    {
+        $user = User::where('id', $request->id)->update([
+            'fname' => $request->fname,
+            'lname' => $request->lname,
+            'mname' => $request->mname,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'username' => $request->username,
+            'gender' => $request->gender
+        ]);
+        return true;
+    }
 }
